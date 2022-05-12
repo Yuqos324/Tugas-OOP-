04b2fd2af4e66f8fa0167dca381262a2b04dfeca
Yuqos324 <102912701+Yuqos324@users.noreply.github.com>
2022-05-12 20:26:32 +0700

    Tugas Polymorphism

--- Main.java
+++ Main.java
@@ -1,41 +1,26 @@
 package com.company;
 
-class MerekHp {
-    void penjelasan(){
-        System.out.println("Inilah harga Hp yang sedang kami jual");
+class Bahasa{
+    public void obrolan(){
+        System.out.println("Apa kabar?");
     }
 }
 
-class Oppo extends MerekHp{
-    int harga1= 1500000;
-
+class BahasaJawa extends Bahasa{
+    public void obrolan(){
+        System.out.println("Piye Kabare?");
+    }
 }
-
-class Vivo extends MerekHp{
-    int harga2 = 2000000;
-}
-
-class Realme extends MerekHp{
-    int harga3 = 1700000;
+class BahasaSunda extends Bahasa{
+    public void obrolan(){
+        System.out.println("Kumaha damang?");
+    }
 }
 
 public class Main {
 
-    public static void main(String[] args){
-
-        MerekHp toko = new MerekHp();
-        toko.penjelasan();
-
-        System.out.println();
-
-        Oppo hp_oppo = new Oppo();
-        System.out.println("Harga HP bermerek OPPO: Rp "+hp_oppo.harga1);
-
-        Vivo hp_vivo = new Vivo();
-        System.out.println("Harga HP bermerek Vivo: Rp "+hp_vivo.harga2);
-
-        Realme hp_realme = new Realme();
-        System.out.println("Harga HP bermerek Realme: Rp "+hp_realme.harga3);
-
+    public static void main(String[] args) {
+        BahasaJawa bahasa = new BahasaJawa();
+        bahasa.obrolan();
     }
 }
